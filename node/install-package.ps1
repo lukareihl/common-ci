d15a01a2ac13132b0bd48a275138d5c2810941eb
romansertsov <roman.sertsov@postindustria.com>
2023-10-30 15:44:03 +0200

    install package script reworked for publish action

--- node/install-package.ps1
+++ node/install-package.ps1
@@ -1,11 +1,56 @@
 param (
     [Parameter(Mandatory=$true)]
-    [string]$RepoName
+    [string]$RepoName,
+    [Parameter(Mandatory=$true)]
+    [array]$Packages
 )
 
 Push-Location $RepoName
 
-npm install (Get-ChildItem -Path ../package -Filter *.tgz) || $(throw "npm install failed")
+# Path to packed packages
+$packageFolderPath = "../package";
 
-Pop-Location
+try {
+    foreach ($package in $Packages) {
+        $path = Join-Path . $package
+        Push-Location $path
+        Write-Output "Switching dependencies for $package"
+
+        $filePath = "package.json"
+
+        # Read the content of the package.json file
+        $jsonContent = Get-Content $filePath | ConvertFrom-Json
+
+        # Loop through dependencies and check if they start with "file:../"
+        foreach ($dependency in $jsonContent.dependencies.PSObject.Properties) {
+            $dependencyName = $dependency.Name
+            $dependencyValue = $dependency.Value
+
+            if ($dependencyValue -match "^file:\.\./(.+)") {
+                $dependencyFileName = $Matches[1]
+
+                # Get all .tgz files in the package folder that match the dependency name
+                $tgzFiles = Get-ChildItem -Path $packageFolderPath -Filter "$dependencyFileName-*.tgz"
+
+                if ($tgzFiles.Count -gt 0) {
+                    # Extract version from the first matching .tgz file
+                    $tgzFileName = $tgzFiles[0].Name
+                    $version = [System.IO.Path]::GetFileNameWithoutExtension($tgzFileName) -replace "$dependencyFileName-", ""
+                    $newDependencyValue = "file:../../package/$dependencyFileName-$version.tgz"
+                    $jsonContent.dependencies.$dependencyName = $newDependencyValue
+                }
+            }
+        }
+
+        # Convert the updated object back to JSON and write it to the file
+        $jsonContent | ConvertTo-Json | Set-Content $filePath
+
+        # Reinstall packages to be sure that we use our packet packages
+        npm install
+
+        Pop-Location
+    }
+} finally {
+    Pop-Location
+}
 
